8364d931d5fbad0b44ecad70fc761ef6bfc64e93
Juan Hoyos <juanhoyos1347@gmail.com>
2022-05-22 15:57:41 -0500

    creacion de sericios de series

---
+++ src/routes/series.router.ts
@@ -0,0 +1,133 @@
+import express, { Request, Response } from 'express';
+import { ObjectId } from 'mongodb';
+import { collections } from "../services/database.service";
+import { createValidator } from 'express-joi-validation';
+
+//Schemas
+import serieSchema from '../schemas/serie.schema'
+
+//Validator schema Joi
+const validator = createValidator({
+    passError:true
+});
+
+
+export const seriesRouter = express.Router();
+
+//Middlewares
+seriesRouter.use(express.json());
+
+seriesRouter.get("/", async (_req: Request, res: Response) => {
+    try {
+        const serie = await collections.series.find({}).toArray();
+
+        res.status(200).send(serie);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+// Example route: http://localhost:8080/games/610aaf458025d42e7ca9fcd0
+seriesRouter.get("/:id", async (req: Request, res: Response) => {
+    const id = req?.params?.id;
+
+    try {
+        // _id in MongoDB is an objectID type so we need to find our specific document by querying
+        const query = { _id: new ObjectId(id) };
+        const serie = await collections.series.findOne(query);
+
+        if (serie) {
+            res.status(200).send(serie);
+        }
+    } catch (error) {
+        res.status(404).send(`Unable to find matching document with id: ${req.params.id}`);
+    }
+});
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      games:
+ *          type:object
+ *          properties:
+ *                 name:
+ *                     type:string
+ *                     description: Nombre de la serie
+                   description:
+                       type:string
+ *                     description: Nombre de la descripcion
+                   year:
+                       type:number
+ *                     description: Año de publicada la serie
+                   gender:
+                       type:string
+ *                     description: Genero de la serie
+ * 
+ * 
+ * /series/
+ *  post:
+ *      sumary:Crear o agregar un juego.
+ *      tags:[games]
+ *      responses:
+ *          200:
+ *              description: Se creo correctalmente la serie
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                 schema:
+ *                   type: object
+ *                   $ref: '#/components/schemas/Vehiculo'
+ */
+seriesRouter.post("/",validator.body(serieSchema), async (req: Request, res: Response) => {
+    try {
+        const newSerie = req.body;
+        console.log(newSerie);
+        const result = await collections.series.insertOne(newSerie);
+        result
+            ? res.status(201).send(`Successfully created a new serie with id ${result.insertedId}`)
+            : res.status(500).send("Failed to create a new serie.");
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error.message);
+    }
+});
+
+seriesRouter.put("/:id", async (req: Request, res: Response) => {
+    const id = req?.params?.id;
+
+    try {
+        const updatedSerie = req.body;
+        const query = { _id: new ObjectId(id) };
+        // $set adds or updates all fields
+        const result = await collections.series.updateOne(query, { $set: updatedSerie });
+
+        result
+            ? res.status(200).send(`Successfully updated serie with id ${id}`)
+            : res.status(304).send(`Game with id: ${id} not updated`);
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).send(error.message);
+    }
+});
+
+seriesRouter.delete("/:id", async (req: Request, res: Response) => {
+    const id = req?.params?.id;
+
+    try {
+        const query = { _id: new ObjectId(id) };
+        const result = await collections.series.deleteOne(query);
+
+        if (result && result.deletedCount) {
+            res.status(202).send(`Successfully removed serie with id ${id}`);
+        } else if (!result) {
+            res.status(400).send(`Failed to remove serie with id ${id}`);
+        } else if (!result.deletedCount) {
+            res.status(404).send(`Game with id ${id} does not exist`);
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).send(error.message);
+    }
+});
